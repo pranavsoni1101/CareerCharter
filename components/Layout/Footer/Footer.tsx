5f0c3e88f2ea8baebd3ea7e294525b488eb3b500
Pranav Soni <pranavsoni1101@gmail.com>
2023-12-17 12:48:49 +0530

    Added footer

---
+++ components/Layout/Footer/Footer.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  )
+}
+
+const Footer = () => {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
+          spacing={8}>
+          <Stack spacing={6}>
+            <Box>
+                <Heading size= "md">Career Charter</Heading>
+            </Box>
+            <Text fontSize={'sm'}>© 2023 Career Charter. All rights reserved</Text>
+          </Stack>
+          
+          <Stack align={'flex-start'}>
+            <ListHeader>Product</ListHeader>
+            <Box as="a" href={'#'}>
+              Overview
+            </Box>
+            <Box as="a" href={'#'}>
+              Features
+            </Box>
+            <Box as="a" href={'#'}>
+              Tutorials
+            </Box>
+            <Box as="a" href={'#'}>
+              Pricing
+            </Box>
+            <Box as="a" href={'#'}>
+              Releases
+            </Box>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Box as="a" href={'#'}>
+              About
+            </Box>
+            <Box as="a" href={'#'}>
+              Contact
+            </Box>
+          </Stack>
+          
+          <Stack align={'flex-start'}>
+            <ListHeader>Follow Us</ListHeader>
+            <Box as="a" href={'#'}>
+              Facebook
+            </Box>
+            <Box as="a" href={'#'}>
+              Twitter
+            </Box>
+            <Box as="a" href={'#'}>
+              Dribbble
+            </Box>
+            <Box as="a" href={'#'}>
+              Instagram
+            </Box>
+            <Box as="a" href={'#'}>
+              LinkedIn
+            </Box>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  )
+}
+
+export default Footer;
