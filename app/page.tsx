d4cd2f6721ab466c8f6f68e9f784415734a8b644
Pranav Soni <pranavsoni1101@gmail.com>
2023-12-18 12:35:19 +0530

    Added a wave svg file and set up landing section of the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,92 @@
-import { Heading } from '@chakra-ui/react';
+'use client'
+
 import React from 'react';
+import Section from '@/components/Section/Section';
+import SectionContent from '@/components/Section/SectionContent';
+import { Flex, Button, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+
+// import './Home.css'
 
 const Home = () => {
     return(
         <>
-            <Heading>Hiii</Heading>
+        <Section>
+            <SectionContent>
+                <Heading
+                    // lineHeight="1.2em"
+                    // w         = "xl"
+                    size      = "4xl"
+                    textAlign = "center"
+                >
+                    Guiding Your 
+                    <Text
+                        as="span"
+                        // color= "#fc0349"
+                        position= "relative"
+                        _after={{
+                            content: "''",
+                            width: 'full',
+                            height: '30%',
+                            position: 'absolute',
+                            bottom: 1,
+                            left: 0,
+                            bg: '#fc0349',
+                            zIndex: -1,
+                          }}
+                    > 
+                        &nbsp;Career&nbsp;
+                    </Text>
+                    Journey: Charting Paths, Nurturing 
+                    <Text
+                        as="span"
+                        // color= "#fc0349"
+                        position= "relative"
+                        _after={{
+                            content: "''",
+                            width: 'full',
+                            height: '30%',
+                            position: 'absolute',
+                            bottom: 1,
+                            left: 0,
+                            bg: '#03ebfc',
+                            zIndex: -1,
+                        }}  
+                    >
+                        &nbsp;Futures&nbsp;
+                    </Text>
+                </Heading>
+                <Text
+                    mt        = "1rem"
+                    fontSize  = "xl"
+                    textAlign = "center"
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maiores numquam unde ipsum veritatis architecto! Harum 
+                    eos inventore dignissimos non! Laboriosam deserunt alias 
+                    velit totam dolor molestias vero repudiandae maxime hic!
+                </Text>
+                <Flex
+                    justifyContent= "center"
+                >
+                    <Button
+                        w  = "sm"
+                        mt = "1rem"
+                        color={useColorModeValue("#fc0349", "#03ebfc")}
+                        variant= "outline"
+                        borderRadius = "xl"
+                        border= {useColorModeValue("2px solid #fc0349", "2px solid #03ebfc")}
+                        transition= "0.6s linear"
+                        _hover={{
+                            border: "none",
+                            color: "#fff",
+                            background: useColorModeValue("#fc0349", "#03ebfc")
+                        }}
+                    >
+                        Learn More
+                    </Button>
+                </Flex>
+            </SectionContent>
+        </Section>
         </>
     )
 }
---
+++ components/WaveSVG.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WaveSVG = () => {
+    return(
+        <>
+            <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 490" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150"><defs><linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%"><stop offset="5%" stop-color="#F78DA7"></stop><stop offset="95%" stop-color="#8ED1FC"></stop></linearGradient></defs><path d="M 0,500 L 0,125 C 133.21428571428572,125.57142857142857 266.42857142857144,126.14285714285714 394,130 C 521.5714285714286,133.85714285714286 643.5000000000001,141 758,151 C 872.4999999999999,161 979.5714285714284,173.8571428571429 1092,170 C 1204.4285714285716,166.1428571428571 1322.2142857142858,145.57142857142856 1440,125 L 1440,500 L 0,500 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="0.53" className="transition-all duration-300 ease-in-out delay-150 path-0"></path><defs><linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%"><stop offset="5%" stop-color="#F78DA7"></stop><stop offset="95%" stop-color="#8ED1FC"></stop></linearGradient></defs><path d="M 0,500 L 0,291 C 92.82142857142858,298.2142857142857 185.64285714285717,305.42857142857144 320,299 C 454.35714285714283,292.57142857142856 630.25,272.5 751,262 C 871.75,251.5 937.3571428571429,250.57142857142856 1043,257 C 1148.642857142857,263.42857142857144 1294.3214285714284,277.2142857142857 1440,291 L 1440,500 L 0,500 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="1" className="transition-all duration-300 ease-in-out delay-150 path-1"></path></svg>
+        </>
+    )
+}
+
+export default WaveSVG;
