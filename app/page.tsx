11c45832783e1e7b2941b819b331bf1ab4a6b426
Pranav Soni <pranavsoni1101@gmail.com>
2023-12-18 14:07:23 +0530

    Created the home page landing layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,72 +3,85 @@
 import React from 'react';
 import Section from '@/components/Section/Section';
 import SectionContent from '@/components/Section/SectionContent';
-import { Flex, Button, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Button, Heading, Text, useColorModeValue, Box,Container, VStack } from '@chakra-ui/react';
+import WaveSVG from '@/components/WaveSVG';
 
 // import './Home.css'
 
 const Home = () => {
     return(
         <>
-        <Section>
-            <SectionContent>
-                <Heading
-                    // lineHeight="1.2em"
-                    // w         = "xl"
-                    size      = "4xl"
-                    textAlign = "center"
-                >
-                    Guiding Your 
-                    <Text
-                        as="span"
-                        // color= "#fc0349"
-                        position= "relative"
-                        _after={{
-                            content: "''",
-                            width: 'full',
-                            height: '30%',
-                            position: 'absolute',
-                            bottom: 1,
-                            left: 0,
-                            bg: '#fc0349',
-                            zIndex: -1,
-                          }}
-                    > 
-                        &nbsp;Career&nbsp;
-                    </Text>
-                    Journey: Charting Paths, Nurturing 
-                    <Text
-                        as="span"
-                        // color= "#fc0349"
-                        position= "relative"
-                        _after={{
-                            content: "''",
-                            width: 'full',
-                            height: '30%',
-                            position: 'absolute',
-                            bottom: 1,
-                            left: 0,
-                            bg: '#03ebfc',
-                            zIndex: -1,
-                        }}  
-                    >
-                        &nbsp;Futures&nbsp;
-                    </Text>
-                </Heading>
-                <Text
-                    mt        = "1rem"
-                    fontSize  = "xl"
-                    textAlign = "center"
-                >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Maiores numquam unde ipsum veritatis architecto! Harum 
-                    eos inventore dignissimos non! Laboriosam deserunt alias 
-                    velit totam dolor molestias vero repudiandae maxime hic!
-                </Text>
+            <Container
+                p    = "0"
+                h = "80vh"
+                maxH = "100vh"
+                maxW = "100%"
+                position= "relative"
+            >
                 <Flex
+                    mt= "5%"
+                    wrap= "wrap"      
+                    align= "center"
+                    alignContent= "center"
                     justifyContent= "center"
+                    p = "2em"
                 >
+                    <Heading
+                        // lineHeight="1.2em"
+                        w         = "4xl"
+                        size      = "4xl"
+                        textAlign = "center"
+                    >
+                        Guiding Your 
+                        <Text
+                            as="span"
+                            // color= "#fc0349"
+                            position= "relative"
+                            _after={{
+                                content: "''",
+                                width: 'full',
+                                height: '30%',
+                                position: 'absolute',
+                                bottom: 1,
+                                left: 0,
+                                bg: '#fc0349',
+                                zIndex: -1,
+                            }}
+                        > 
+                            &nbsp;Career&nbsp;
+                        </Text>
+                        Journey: Charting Paths, Nurturing 
+                        <Text
+                            as="span"
+                            // color= "#fc0349"
+                            position= "relative"
+                            _after={{
+                                content: "''",
+                                width: 'full',
+                                height: '30%',
+                                position: 'absolute',
+                                bottom: 1,
+                                left: 0,
+                                bg: '#03ebfc',
+                                zIndex: -1,
+                            }}  
+                        >
+                            &nbsp;Futures&nbsp;
+                        </Text>
+                    </Heading>
+                    <Text
+                        w         = "6xl"
+                        mt        = "1rem"
+                        fontSize  = "xl"
+                        textAlign = "center"
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Maiores numquam unde ipsum veritatis architecto! Harum 
+                        eos inventore dignissimos non! Laboriosam deserunt alias 
+                        velit totam dolor molestias vero repudiandae maxime hic!
+                    </Text>
                     <Button
+                        order={3}
                         w  = "sm"
                         mt = "1rem"
                         color={useColorModeValue("#fc0349", "#03ebfc")}
@@ -85,8 +98,16 @@ const Home = () => {
                         Learn More
                     </Button>
                 </Flex>
-            </SectionContent>
-        </Section>
+                <Box
+                    position= "absolute"
+                    // mt= "2em"
+                    bottom= "0"
+                    w = "100%"
+                    zIndex= "-2"
+                >
+                    <WaveSVG />
+                </Box>
+            </Container>
         </>
     )
 }
