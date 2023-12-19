eb5d4c3945d763d74f3538bfb4033fe6125bcf57
Pranav Soni <pranavsoni1101@gmail.com>
2023-12-19 15:37:57 +0530

    Changed the footer background

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -23,8 +23,9 @@ const ListHeader = ({ children }: { children: ReactNode }) => {
 const Footer = () => {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      backgroundImage="linear-gradient(45deg, #8ED1FC, #F78DA7)"
+      color={useColorModeValue('gray.700', 'gray.200')}  
+    >
       <Container as={Stack} maxW={'6xl'} py={10}>
         <SimpleGrid
           templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
