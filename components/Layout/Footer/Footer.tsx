4d605bbe82f9567d662bca9e1c484de181f62182
Pranav Soni <pranavsoni1101@gmail.com>
2023-12-20 11:26:00 +0530

    Added a divider in the footer section

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from 'react'
 import {
   Box,
   Container,
+  Divider,
   Heading,
   SimpleGrid,
   Stack,
@@ -22,72 +23,81 @@ const ListHeader = ({ children }: { children: ReactNode }) => {
 
 const Footer = () => {
   return (
-    <Box
-      backgroundImage="linear-gradient(45deg, #8ED1FC, #F78DA7)"
-      color={useColorModeValue('gray.700', 'gray.200')}  
-    >
-      <Container as={Stack} maxW={'6xl'} py={10}
-            color= "black"
+    <>
+      <Divider 
+        h = "3px"
+        w = "xl"
+        m = "2.5em auto"
+        backgroundImage="linear-gradient(45deg, #F78DA7, #8ED1FC)"
+
+      />
+      <Box
+        backgroundImage="linear-gradient(45deg, #8ED1FC, #F78DA7)"
+        color={useColorModeValue('gray.700', 'gray.200')}  
       >
-        <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
-          spacing={8}>
-          <Stack spacing={6}>
-            <Box>
-                <Heading size= "md">Career Charter</Heading>
-            </Box>
-            <Text fontSize={'sm'}>© 2023 Career Charter. All rights reserved</Text>
-          </Stack>
-          
-          <Stack align={'flex-start'}>
-            <ListHeader>Product</ListHeader>
-            <Box as="a" href={'#'}>
-              Overview
-            </Box>
-            <Box as="a" href={'#'}>
-              Features
-            </Box>
-            <Box as="a" href={'#'}>
-              Tutorials
-            </Box>
-            <Box as="a" href={'#'}>
-              Pricing
-            </Box>
-            <Box as="a" href={'#'}>
-              Releases
-            </Box>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Company</ListHeader>
-            <Box as="a" href={'#'}>
-              About
-            </Box>
-            <Box as="a" href={'#'}>
-              Contact
-            </Box>
-          </Stack>
-          
-          <Stack align={'flex-start'}>
-            <ListHeader>Follow Us</ListHeader>
-            <Box as="a" href={'#'}>
-              Facebook
-            </Box>
-            <Box as="a" href={'#'}>
-              Twitter
-            </Box>
-            <Box as="a" href={'#'}>
-              Dribbble
-            </Box>
-            <Box as="a" href={'#'}>
-              Instagram
-            </Box>
-            <Box as="a" href={'#'}>
-              LinkedIn
-            </Box>
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+        <Container as={Stack} maxW={'6xl'} py={10}
+              color= "black"
+        >
+          <SimpleGrid
+            templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
+            spacing={8}>
+            <Stack spacing={6}>
+              <Box>
+                  <Heading size= "md">Career Charter</Heading>
+              </Box>
+              <Text fontSize={'sm'}>© 2023 Career Charter. All rights reserved</Text>
+            </Stack>
+            
+            <Stack align={'flex-start'}>
+              <ListHeader>Product</ListHeader>
+              <Box as="a" href={'#'}>
+                Overview
+              </Box>
+              <Box as="a" href={'#'}>
+                Features
+              </Box>
+              <Box as="a" href={'#'}>
+                Tutorials
+              </Box>
+              <Box as="a" href={'#'}>
+                Pricing
+              </Box>
+              <Box as="a" href={'#'}>
+                Releases
+              </Box>
+            </Stack>
+            <Stack align={'flex-start'}>
+              <ListHeader>Company</ListHeader>
+              <Box as="a" href={'#'}>
+                About
+              </Box>
+              <Box as="a" href={'#'}>
+                Contact
+              </Box>
+            </Stack>
+            
+            <Stack align={'flex-start'}>
+              <ListHeader>Follow Us</ListHeader>
+              <Box as="a" href={'#'}>
+                Facebook
+              </Box>
+              <Box as="a" href={'#'}>
+                Twitter
+              </Box>
+              <Box as="a" href={'#'}>
+                Dribbble
+              </Box>
+              <Box as="a" href={'#'}>
+                Instagram
+              </Box>
+              <Box as="a" href={'#'}>
+                LinkedIn
+              </Box>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </>
   )
 }
 
